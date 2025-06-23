95c6ffedfd68d2b8463b7a75357b8900a580a133
ljh8159 <junhag6107@gmail.com>
2025-06-24 02:42:43 +0900

    Flask 백엔드 구현 완료

--- app/models/user.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(255))
-    role = db.Column(db.String(20))  # '청년', '어르신', '코디네이터'
