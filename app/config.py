ad801d08bd1a0b2c1215a129a62fbced7c385081
rimeeeee <innt99@gmail.com>
2025-06-29 03:09:50 +0900

    db수정

--- app/config.py
+++ app/config.py
@@ -6,17 +6,12 @@ load_dotenv()
 
 class Config:
     # 로컬 테스트용 SQLite 설정
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
     
     # PostgreSQL 설정 (주석 처리)
-    # SQLALCHEMY_DATABASE_URI = (
-    #     f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
-    #     f"@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}?sslmode={os.environ.get('DB_SSL_MODE')}"
-    # )
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'super-secret-key'
     SECRET_KEY = 'super-secret-key'
-    
     # templates 디렉토리 설정
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates')
